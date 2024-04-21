616d61188afde5b07cb6bdde2c386dd19856a37f
ReackMan <reackman@yandex.ru>
2024-04-22 01:10:24 +0300

    add index.ts for assets/icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { CheckIcon } from './components/checkIcon'
+export { ClosedEyeIcon } from './components/closedEyeIcon'
+export { OpenEyeIcon } from './components/openEyeIcon'
+export { SearchIcon } from './components/searchIcon'
